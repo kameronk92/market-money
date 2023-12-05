5dd2c5664bc55fbb89011a1da213cb4e096a3bf2
Kam Kennedy <kameronk013@gmail.com>
2023-12-04 20:51:26 -0700

    tests passing

--- spec/requests/api/v0/market_requrest_spec.rb
+++ spec/requests/api/v0/market_requrest_spec.rb
@@ -7,10 +7,12 @@ describe "Markets API" do
     get '/api/v0/markets'
 
     expect(response).to be_successful
+
     parsed_response = JSON.parse(response.body, symbolize_names: true)
 
-    expect(parsed_response).to be_a(Hash)
-    expect(parsed_response).to have_key('market')
-    expect(parsed_response['market']).to be_a(string)
+    expect(parsed_response).to be_an(Array)
+    expect(parsed_response.first).to be_a(Hash)
+    expect(parsed_response.first).to have_key(:name)
+    expect(parsed_response.first[:name]).to be_a(String)
   end
 end
