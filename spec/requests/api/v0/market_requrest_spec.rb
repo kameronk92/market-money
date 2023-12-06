d139b15bd04c19d2944f2abe6530db6accf66a46
Kam Kennedy <kameronk013@gmail.com>
2023-12-06 10:17:22 -0700

    market show sad path passing

--- spec/requests/api/v0/market_requrest_spec.rb
+++ spec/requests/api/v0/market_requrest_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe "Markets Index" do
-  it "sends a list of all markets" do
+  it "sends a list of all markets , happy" do
     create_list(:market, 7)
 
     @vendors = create_list(:vendor, 20) 
@@ -52,7 +52,7 @@ describe "Markets Index" do
     end
   end
 
-  it "sends details of a single market" do
+  it "sends details of a single market, happy" do
     market = create(:market)
 
     get api_v0_market_path(market.id)
@@ -60,5 +60,61 @@ describe "Markets Index" do
     expect(response).to be_successful
 
     market = JSON.parse(response.body, symbolize_names: true)
+
+    expect(market).to have_key(:data)
+    expect(market[:data]).to be_a(Hash)
+
+    expect(market[:data]).to have_key(:id)
+    expect(market[:data][:id]).to be_a(String)
+    
+    expect(market[:data]).to have_key(:type)
+    expect(market[:data][:type]).to be_a(String)
+
+    expect(market[:data]).to have_key(:attributes)
+    expect(market[:data][:attributes]).to be_a(Hash)
+
+    attributes = market[:data][:attributes]
+
+    expect(attributes).to have_key(:name)
+    expect(attributes[:name]).to be_a(String)
+
+    expect(attributes).to have_key(:street)
+    expect(attributes[:street]).to be_a(String)
+
+    expect(attributes).to have_key(:city)
+    expect(attributes[:city]).to be_a(String)
+    
+    expect(attributes).to have_key(:county)
+    expect(attributes[:county]).to be_a(String)
+
+    expect(attributes).to have_key(:state)
+    expect(attributes[:state]).to be_a(String)
+    
+    expect(attributes).to have_key(:zip)
+    expect(attributes[:zip]).to be_a(String)
+    
+    expect(attributes).to have_key(:lat)
+    expect(attributes[:lat]).to be_a(String)
+
+    expect(attributes).to have_key(:lon)
+    expect(attributes[:lon]).to be_a(String)
+
+    expect(attributes).to have_key(:vendor_count)
+    expect(attributes[:vendor_count]).to be_a(Integer) 
+  end
+
+  it "sends details of a single market, sad" do
+    expect Market.count == 0
+
+    get api_v0_market_path(1)
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(404)
+
+    data = JSON.parse(response.body, symbolize_names: true)
+
+    expect(data[:errors]).to be_a(Array)
+    expect(data[:errors].first[:status]).to eq("404")
+    expect(data[:errors].first[:title]).to eq("Couldn't find Market with 'id'=1")
   end
 end
